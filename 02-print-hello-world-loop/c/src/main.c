53859dc03c6eec5a4073469c406164798b11f6ca
yuneta <eduardo.mollejo@mulesol.es>
2023-01-09 12:26:36 +0100

    Initial commit for project 02

---
+++ 02-print-hello-world-loop/c/src/main.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include <stdint.h>>
+#include <time.h>
+
+int main(int argc, char *argv[])
+{
+
+    int n;
+    if(argc < 2){
+        n = 100;
+    }
+    else {
+        n = atoi(argv[1]);
+    }
+
+    if(n <= 0){
+        printf("Invalid argument.");
+    }
+
+    clock_t clockEnd;
+    clock_t clockStart = clock();
+
+    for (size_t i = 0; i < n; i++)
+    {
+        printf("Hello world\n");
+    }
+    clockEnd = clock();
+    printf("Time elapsed: %.2f seconds\n", (double)(clockEnd - clockStart) / CLOCKS_PER_SEC);
+    printf("Lines printed: %.i\n",n);
+    return 0;
+}
