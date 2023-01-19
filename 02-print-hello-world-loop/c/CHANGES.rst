aaf2bf43e6e52192af2b3a2f74a6beaed378cc94
AnOtga <119301466+AnOtga@users.noreply.github.com>
2023-01-19 13:29:50 +0100

    Created proj 03, fixed parsing issues in 02

--- 02-print-hello-world-loop/c/CHANGES.rst
+++ 02-print-hello-world-loop/c/CHANGES.rst
@@ -1,5 +1,9 @@
 Changes
 =======
+v0.4
+----
+- Fixed the program mistakenly parsing arguments a second time, which led to a unintended behaviour when using proper ARGP parameters
+
 v0.3
 ----
 - Now features help for arguments and docs
--- 03-print-hello-parametername/c/src/main.c
+++ 03-print-hello-parametername/c/src/main.c
@@ -9,30 +9,34 @@
 /***************************************************************************
  *      Data
  ***************************************************************************/
-const char *argp_program_version = "Hello_World_Loop 0.4";
+const char *argp_program_version = "Hello_World_ParameterName 0.1";
 const char *argp_program_bug_address = "<example@example.com>";
 
 /* Program documentation. */
-static char doc[] = "";
+static char doc[] = "Prints a specified number of lines containing 'Hello <name>', where <name> is a required argument.";
 
-/* A description of the arguments we accept. */
-static char args_doc[] = "";
+/* A description of the required must-have arguments we accept. */
+static char args_doc[] = "name";
 
 /*
  *  The options we understand.
  *  See https://www.gnu.org/software/libc/manual/html_node/Argp-Option-Vectors.html
  */
 static struct argp_option options[] = {
-{"lines", 'l', "LINES", 0, "Number of lines to print"},
+{"lines", 'l', "LINES", 0, "Number of lines to print, defaults to 100", 0},
 {0}
 };
 
 /* Used by main to communicate with parse_opt*/
 struct arguments {
-    int lines;
+    uint64_t lines;
+    char* name;
 };
 
-/* Parse a single option */
+/*
+ * Parse a single option
+ * See https://www.gnu.org/software/libc/manual/html_node/Argp-Helper-Functions.html for more information
+ */
 static error_t parse_opt (int key, char *arg, struct argp_state *state)
 {
     /*
@@ -48,15 +52,30 @@ static error_t parse_opt (int key, char *arg, struct argp_state *state)
         }
         break;
 
-    case ARGP_KEY_ARG:
-        return 0;
+    case ARGP_KEY_ARG: {
+        //Parse arg at index 0 (?)
+        if(state->arg_num == 0) {
+            arguments->name = arg;
+        }
+        else {
+            //Something seems to have went wrong, exit.
+            argp_usage(state);
+        }
+        break;
+    }
 
-    case ARGP_KEY_END:
-        return 0;
+    case ARGP_KEY_END: {
+        // We have reached the end of the arguments, validate them
+        if(arguments->name == NULL) {
+            argp_error(state, "Required argument NAME is null!");
+        }
+        break;
+    }
 
     default:
         return ARGP_ERR_UNKNOWN;
     }
+
     return 0;
 }
 
@@ -70,7 +89,7 @@ static struct argp argp = {
 
 
 /***************************************************************************
- *      Main Program
+ *      Main program
  ***************************************************************************/
 static inline struct timespec ts_diff (struct timespec start, struct timespec end)
 {
@@ -89,39 +108,24 @@ int main(int argc, char* argv[])
 {
     struct arguments arguments;
     memset(&arguments, 0, sizeof(arguments));
-    // Default values
+    //Set Default values
     arguments.lines = 100;
     //And parse the arguments
     argp_parse(&argp, argc, argv, 0, 0, &arguments);
 
-    uint64_t n = arguments.lines;
     struct timespec st, et, tt;
-
-    //Manual argument parsing.
-    //"Legacy" code whose only purpose is to serve as a example, do not uncomment, as it is incompatible with ARGP parsing.
-    /*
-    if (argc < 2) {
-        n = 100;
-    }
-    else {
-        n = atoi(argv[1]);
-    }
-
-    if (n <= 0) {
-        printf("Invalid argument.");
-    }*/
 
     clock_gettime(CLOCK_MONOTONIC, &st);
 
-    for (size_t i = 0; i < n; i++) {
-        printf("Hello world\n");
+    for (size_t i = 0; i < arguments.lines; i++) {
+        printf("Hello %s\n", arguments.name);
     }
 
     clock_gettime(CLOCK_MONOTONIC, &et);
 
     tt = ts_diff(st, et);
     printf("Time elapsed: %lu.%lus seconds\n", tt.tv_sec, tt.tv_nsec);
-    printf("Lines printed: %lu\n", n);
+    printf("Lines printed: %lu\n", arguments.lines);
 
     return 0;
 }
