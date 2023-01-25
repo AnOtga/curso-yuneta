4be419014e4b8c0f14cecba5523f892ddf38eb7c
AnOtga <119301466+AnOtga@users.noreply.github.com>
2023-01-25 15:25:46 +0100

    Changed js of prj02

--- 02-print-hello-world-loop/js/src/main.js
+++ 02-print-hello-world-loop/js/src/main.js
@@ -1,26 +1,35 @@
 #!/usr/bin/env node
 
-const argv = require('minimist')(process.argv.slice(2));
-let lines = argv._[0];
+const program = require('commander');
+
+program
+.description('Command Line Interface Hello World')
+.usage('[-h] [-v] [-l LINES]')
+.option('-l, --lines <int>', 'Repeat execution "Hello World" times.', 100, parseInt)
+.version('Hello World Loop 1.0.0', '-v, --version')
+.parse(process.argv);
+
+const options = program.opts();
 
 /***************************************************************************
  *      Main Program
  ***************************************************************************/
 function main() {
-    if (!lines) {
-        lines = 100;
+    if (options == undefined || !options.lines == undefined) {
+        console.log(`Error parsing options`);
+        return 0;
     }
 
     const st = new Date();
 
-    for (let i = 0; i < lines; i++) {
+    for (let i = 0; i < options.lines; i++) {
         console.log(`Hello World`);
     }
 
     const et = new Date();
     const tt = (et.getTime() - st.getTime()) / 1000;
 
-    console.log(`Time taken to execute ${lines} prints: ${tt}`);
+    console.log(`Time taken to execute ${options.lines} prints: ${tt}`);
 }
 
 main();
