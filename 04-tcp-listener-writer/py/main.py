f2431e3534eb498d695d0dd6ad08a204cbd593e2
AnOtga <119301466+AnOtga@users.noreply.github.com>
2023-01-30 12:03:43 +0100

    "Changes in main.py"

--- 04-tcp-listener-writer/py/main.py
+++ 04-tcp-listener-writer/py/main.py
@@ -5,16 +5,11 @@ import socket
 
 program_version = "TCP Writer 0.0.1"
 
-parser = argparse.ArgumentParser(
-    description='Connects to a server address through the 8000 port.')
-parser.add_argument("-v", "--version", help="Print program version",
-                    action="version", version=program_version)
-parser.add_argument(
-    "--ip", "-i", help="IP to connect to", default="127.0.0.1", type=str)
-parser.add_argument(
-    "--port", "-p", help="Port to connect to", default=8000, type=int)
-parser.add_argument(
-    "MESSAGE", help="Message to send through", type=str)
+parser = argparse.ArgumentParser(description='Connects to a server address through the 8000 port.')
+parser.add_argument("-v", "--version", help="Print program version", action="version", version=program_version)
+parser.add_argument("--ip", "-i", help="IP to connect to", default="127.0.0.1", type=str)
+parser.add_argument("--port", "-p", help="Port to connect to", default=8000, type=int)
+parser.add_argument("MESSAGE", help="Message to send through", type=str)
 args = parser.parse_args()
 
 #######################################################
